5d69228322224676e34e25a84ea860642ded783c
Jack Dempsey <jack.dempsey@gmail.com>
2008-12-04 11:20:29 -0500

    add the first pieces in

--- lib/sequel_polymorphic/sequel_polymorphic.rb
+++ lib/sequel_polymorphic/sequel_polymorphic.rb
@@ -3,9 +3,25 @@ module Sequel
     module Polymorphic
       # Apply the plugin to the model.
       def self.apply(model, options = {})
+        if as_variable = (options[:belongs_to] or options[:many_to_one])
+          # defining the polymorphic model
+          model.class_eval %{associate(:many_to_one, #{as_variable}, :reciprocal=>:assets, 
+            :dataset=>(proc { klass = attachable_type.constantize; klass.filter(klass.primary_key=>attachable_id) }), 
+            :eager_loader=>(proc do |key_hash, assets, associations|
+              id_map = {}
+              assets.each { |asset| asset.associations[:attachable] = nil; ((id_map[asset.attachable_type] ||= {})[asset.attachable_id] ||= []) << asset }
+              id_map.each do |klass_name, id_map|
+                klass = klass_name.constantize
+                klass.filter(klass.primary_key=>id_map.keys).all do |attach|
+                  id_map[attach.pk].each { |asset| asset.associations[:attachable] = attach }
+                end
+              end
+            end))}
+        end
       end
 
       module InstanceMethods
+
       end
 
       module ClassMethods
@@ -13,4 +29,16 @@ module Sequel
     end # Polymorphic
   end # Plugins
 end # Sequel
-    
+
+# class Asset < Sequel::Model
+#   is :polymorphic, :belongs_to => :attachable
+# end
+# 
+# class Post < Sequel::Model
+#   is :polymorphic, :has_many => :assets, :as => :attachable
+# end
+# 
+# class Note < Sequel::Model
+#   is :polymorphic, :has_many => :assets, :as => :attachable
+# end
+
