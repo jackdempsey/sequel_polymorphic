15adc03abc518714b5676dc61757e1b75fca35f6
Kurakin Alexander <kuraga333@mail.ru>
2015-06-09 23:29:46 +0300

    Release version 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 0.2.0 (unreleased)
+# 0.2.0 (2015-06-10)
 
 * *Many-to-one associations breaking change*.
 
--- lib/sequel_polymorphic/version.rb
+++ lib/sequel_polymorphic/version.rb
@@ -2,7 +2,7 @@ module Sequel
   module Plugins
     module Polymorphic
 
-      VERSION = '0.1.1'
+      VERSION = '0.2.0'
 
     end # Polymorphic
   end # Plugins
